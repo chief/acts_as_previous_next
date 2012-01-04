c3a7565bd2e3ab61d0ef84748ede810a58ab8bb1
Giorgos Tsiftsis <giorgos.tsiftsis@generation-y.gr>
2012-01-04 19:38:34 +0200

    Add concern support

--- lib/acts_as_previous_next.rb
+++ lib/acts_as_previous_next.rb
@@ -1,2 +1,12 @@
+require 'active_support/concern'
+
 module ActsAsPreviousNext
+  extend ActiveSupport::Concern
+
+  included do
+
+    def self.acts_as_previous_next(*options)
+    end
+
+  end
 end
